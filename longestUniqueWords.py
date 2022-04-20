1cf78157fd388380950f8daf950fd746d4d81b45
Nishank Rajvanshi <nishank.rajvanshi@gmail.com>
2022-04-20 17:11:26 +0530

    First commit

--- longestUniqueWords.py
+++ longestUniqueWords.py
@@ -1,5 +1,17 @@
 def get_n_longest_unique_words(words, n):
-    # Write your code here.
-    wordLenghthDict = {1:"hi",1:"bye"}
-    
+    mul_occurence_set  = set()
+    longest_unique_word_list = []
+    word_list = list(words)
+    for word in word_list:
+        word_count = word_list.count(word)
+        if word_count > 1:
+            mul_occurence_set.add(word)
+    list_removing_duplicates = list(set(word_list) - mul_occurence_set)    
+    list_removing_duplicates.sort(reverse=True, key=sort_on_len)
+    for i in range(n):
+        longest_unique_word_list.append(list_removing_duplicates[i])
+    return longest_unique_word_list
+def sort_on_len(item):
+    return len(item)
+
     
