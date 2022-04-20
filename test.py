9531abf1ee2eeb92e5ca9695bfc05d9df3b967f5
Nishank Rajvanshi <nishank.rajvanshi@gmail.com>
2022-04-20 22:34:38 +0530

    Initial commit

--- test.py
+++ test.py
@@ -1,19 +1,4 @@
-# number = float(input("Enter a number: "))
-# if number > 10 and number < 20:
-#     another_number = float(input("Enter another number: "))
-#     sum = number + another_number
-#     print("The sum of these two nunbers is: " , sum)
-#     if sum > 100:
-#         print("That is a larger sum!")
-
-
-
-number = -2.76521
-print(round(number,2))
-num_tuple = (2,5,9,8)
-num_tuple = sorted(num_tuple)
-print(num_tuple)
-lst = [1,6,2,1,7,5,4]
-#lst.sort(reverse=True)
-sorted(lst,reverse=True)
-print(lst)
+string1 = "Hello"
+string2 = "nishank"
+for char in string1:
+    print(char, end = '\n')
